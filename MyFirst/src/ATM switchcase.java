ee1dac6a3d05acf9390dc9a094967ef25e2c8b49
Cherokee46 <136843504+Cherokee46@users.noreply.github.com>
2023-06-19 14:31:49 +0300

    Update ATM switchcase.java

--- MyFirst/src/ATM switchcase.java
+++ MyFirst/src/ATM switchcase.java
@@ -5,8 +5,11 @@ public class ATMApp
 
     public static void main(String[] args)
         {
-            System.out.println("HELLO World ! \n");
-       
-
+            System.out.println("Enter a Number:  \n");
+            Scanner scan= new Scanner(System.in);
+            int Number= scan.nextInt();
+            int FirstMoney=1000;
+        
+        
         }
 }
